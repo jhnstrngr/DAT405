67f00d20af1fbc38e517a832c570ff0de6426b35
jhnstrngr <john.stringer.martin@gmail.com>
2017-11-28 14:24:55 +0000

    Update sketch.js

--- 03/sketch.js
+++ 03/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 }
 
 function draw() {
-  fill(215, 200, 170);
+
 
   //Run the nested loop for x, and y
   for (let y = 0; y < 10; y++) {
@@ -20,15 +20,8 @@ function draw() {
       //Draw the shape at the corresponding position
       //This will generate a dynamic position for the x and y
       //The shape is redrawn as many times as the loop says (in this case 100)
+      fill(215, 200, 170);
       rect(size * x, size * y, size, size);
-    }
-  }
-
-  for (let y = 0; y < 10; y++) {
-    for (let x = 0; x < 10; x++) {
-      //Draw the shape at the corresponding position
-      //This will generate a dynamic position for the x and y
-      //The shape is redrawn as many times as the loop says (in this case 100)
       fill(random(0,255), random(0,255), random(0,255))
       rect(size * x + size/4, size * y + size/4, size/2, size/2);
     }
